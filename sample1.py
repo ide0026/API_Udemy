b4a517f6244825547ee7dbf284d573b6b35ad5b7
unknown <20120026@kaishi-pu.ac.jp>
2022-01-21 10:52:02 +0900

    first

--- sample1.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from newsapi import NewsApiClient
-#この先ニュース取得
-api = NewsApiClient(api_key="b4db0f52d6fe498e8153750a1ba090f9")
-api = NewsApiClient(api_key="APIキー")
-
-print(api.get_everything(q='大阪'))
